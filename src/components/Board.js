7d2bec9e94b6e1e17dfe2636d0d9b3453d8e79d2
stefanbpeterson <stefanbpeterson@gmail.com>
2022-01-20 21:51:59 -0700

    finishing first version of app

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,18 +4,54 @@ import Square from './Square';
 function Board() {
     const [square, setSquare] = useState(Array(9).fill(null))
     const [X, setX] = useState(true)
-
+    
     const renderSquare = (i) => {
         return <Square value={square[i]} onClick={() => handleClick(i)} />
     }
 
     const handleClick = (i) => {
         const squares = square.slice()
-        squares[i] = X ? 'X' : 'O'
-        setSquare(squares)
-        setX(!X)
+
+        if(squares[i] === null) {
+            squares[i] = X ? 'X' : 'O'
+            setSquare(squares)
+            setX(!X)
+        } else {
+            alert(`Can't do that`)
+        }
     }
 
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        for(let i = 0; i < lines.length; i++) {
+            const [a,b,c] = lines[i]
+
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+
+        return null
+    }
+
+    const winner = calculateWinner(square)
+    let status
+    if(winner) {
+        status = `Winner: ${winner}`
+    } else {
+        status = 'Player turn: ' + (X ? 'X' : 'O')
+    }
+    
     return (
         <div className='board'>
             <div className='board-row'>
@@ -36,6 +72,7 @@ function Board() {
                 {renderSquare(8)}
             </div>
 
+            {status}
             
         </div>
     )
