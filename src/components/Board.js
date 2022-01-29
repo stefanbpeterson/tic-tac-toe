52187a441958792adc0d0fc7b94942260a1de576
stefanbpeterson <stefanbpeterson@gmail.com>
2022-01-29 08:47:40 -0700

    making style changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,7 +13,7 @@ function Board() {
         const squares = square.slice()
 
         if(squares[i] === null) {
-            squares[i] = X ? '🤪' : '😂'
+            squares[i] = X ? '🤪' : '🐱'
             setSquare(squares)
             setX(!X)
         } else {
@@ -49,7 +49,7 @@ function Board() {
     if(winner) {
         status = `${winner} WINS!`
     } else {
-        status = 'Player turn: ' + (X ? '🤪' : '😂')
+        status = 'Player turn: ' + (X ? '🤪' : '🐱')
     }
     
     return (
